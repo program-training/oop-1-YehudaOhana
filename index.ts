bc5d98a6a29ee345d0c5f97fdaa863e79c5f19f5
yehudaOhana <yehuda9955@gmail.com>
2023-08-27 13:40:39 +0300

    start

--- index.ts
+++ index.ts
@@ -1,11 +1,140 @@
-// את הקוד שלכם תכתבו כאן
+// //1
+// class Rectangle {
+//   width: number;
+//   height: number;
+//   constructor(width: number, height: number) {
+//     this.width = width;
+//     this.height = height;
+//   }
+//   area(): number {
+//     return this.width * this.height;
+//   }
+// }
 
-//  1. Rectangle - מלבן
+// //2
+// class Square extends Rectangle {
+//   constructor(width: number) {
+//     super(width, width);
+//   }
+// }
 
-//  2. Square - ריבוע
+// //3
+// class Shape {
+//   constructor() {}
+//   info(): string {
+//     return `This is a Shape`;
+//   }
+// }
 
-// 3. Shape - צורה
+// class Rectangle extends Shape {
+//   width: number;
+//   height: number;
+//   constructor(width: number, height: number) {
+//     super();
+//     this.width = width;
+//     this.height = height;
+//   }
 
-// 4. Method Chaining
+//   info(): string {
+//     return `This is a rectangle`;
+//   }
 
-// 5. Shape with draw method
+//   area(): number {
+//     return this.width * this.height;
+//   }
+// }
+
+// class ColoredRectangle extends Rectangle {
+//   color: string;
+//   constructor(width: number, height: number, color: string) {
+//     super(width, height);
+//     this.color = color;
+//   }
+//   info(): string {
+//     return `This is a rectangle ${this.color}`;
+//   }
+// }
+
+// //4
+// class Shape {
+//   constructor() {}
+//   info(): string {
+//     return `This is a Shape`;
+//   }
+// }
+
+// class Rectangle extends Shape {
+//   width: number;
+//   height: number;
+//   constructor(width: number, height: number) {
+//     super();
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   info(): string {
+//     return `This is a rectangle`;
+//   }
+
+//   area(): number {
+//     return this.width * this.height;
+//   }
+
+//   scale(x: number): this {
+//     this.width *= x;
+//     this.height *= x;
+//     return this;
+//   }
+
+//   static twoRectangle(Rectangle1: Rectangle, Rectangle2: Rectangle): Rectangle {
+//     const newRectangle = new Rectangle(
+//       Rectangle1.width + Rectangle2.width,
+//       Rectangle1.height + Rectangle2.height
+//     );
+//     return newRectangle;
+//   }
+// }
+
+//5
+class Shape {
+  constructor() {}
+  draw(): void {
+    console.log(`drawing a shape`);
+  }
+}
+
+class Rectangle extends Shape {
+  width: number;
+  height: number;
+  constructor(width: number, height: number) {
+    super();
+    this.width = width;
+    this.height = height;
+  }
+
+  info(): string {
+    return `This is a rectangle`;
+  }
+
+  area(): number {
+    return this.width * this.height;
+  }
+
+  scale(x: number): this {
+    this.width *= x;
+    this.height *= x;
+    return this;
+  }
+
+  static twoRectangle(Rectangle1: Rectangle, Rectangle2: Rectangle): Rectangle {
+    const newRectangle = new Rectangle(
+      Rectangle1.width + Rectangle2.width,
+      Rectangle1.height + Rectangle2.height
+    );
+    return newRectangle;
+  }
+}
+
+class Circle extends Shape {
+  co;
+}
